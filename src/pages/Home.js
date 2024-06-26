e94aab28b90c657f7b10c501dba13a07aa53af46
Noble <crhneto@gmail.com>
2024-06-26 09:03:59 -0300

    Só falta add os projetos

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,33 @@
 // src/pages/Home.js
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import "../css/home.css"; // Importe o arquivo de estilos específico para a página Home
 import eu from "../img/eu-profissional.jpg"; // Importe a imagem
 
+
 const Home = () => {
+    const [selectedButton, setSelectedButton] = useState(1); // Estado para controlar o botão selecionado
+
+    // Dados dos projetos por botão
+    const projectsData = {
+        1: [
+            { id: 1, image: "projeto1.jpg", text: "Descrição do Projeto 1" },
+            { id: 2, image: "projeto2.jpg", text: "Descrição do Projeto 2" },
+        ],
+        2: [
+            { id: 3, image: "projeto3.jpg", text: "Descrição do Projeto 3" },
+            { id: 4, image: "projeto4.jpg", text: "Descrição do Projeto 4" },
+        ],
+        3: [
+            { id: 5, image: "projeto5.jpg", text: "Descrição do Projeto 5" },
+            { id: 6, image: "projeto6.jpg", text: "Descrição do Projeto 6" },
+        ],
+    };
+
+    // Função para atualizar o botão selecionado
+    const handleButtonClick = (buttonNumber) => {
+        setSelectedButton(buttonNumber);
+    };
     return (
         <>
             <Container className="home-container">
@@ -20,7 +43,6 @@ const Home = () => {
                         <button className="button-legenda">GitHub</button>
                         <button className="button-legenda">Linkedin</button>
                     </Col>
-
                     <Col md={6} className="right-content">
                         <img src={eu} alt="Minha Foto" className="eu-img" />
                     </Col>
@@ -60,33 +82,120 @@ const Home = () => {
             <Container>
                 <div className='row-skills'>
                     <div className="card-left">
-                        <button className='button-dev-left-titulo'>Front-End</button>
-                        <button className='button-dev-left'>GitHub</button>
-                        <button className='button-dev-left'>GitHub</button>
-                        <button className='button-dev-left'>GitHub</button>
-                        <button className='button-dev-left'>GitHub</button>
-                        <button className='button-dev-left-button'>GitHub</button>
+                        <div className="button-wrapper">
+                            <button className='button-dev-left-titulo'>Front-End</button>
+                            <div className="popup">Tecnologias e ferramentas usadas para criar interfaces de usuário.</div>
+                        </div>
+                        <div className="button-wrapper">
+                            <button className='button-dev-left'>HTML</button>
+                            <div className="popup">Linguagem de marcação para a criação de páginas web.</div>
+                        </div>
+                        <div className="button-wrapper">
+                            <button className='button-dev-left'>CSS</button>
+                            <div className="popup">Estilização de elementos HTML.</div>
+                        </div>
+                        <div className="button-wrapper">
+                            <button className='button-dev-left'>JavaScript</button>
+                            <div className="popup">Linguagem de programação para criar interatividade em páginas web.</div>
+                        </div>
+                        <div className="button-wrapper">
+                            <button className='button-dev-left'>TypeScript</button>
+                            <div className="popup">Uma linguagem de programação que é um superconjunto de JavaScript, adicionando tipagem estática e outros recursos.</div>
+                        </div>
+                        <div className="button-wrapper">
+                            <button className='button-dev-left-button'>SCSS</button>
+                            <div className="popup">Compilador de CSS, usado para estilização de elementos HTML.</div>
+                        </div>
                     </div>
                     <div className="card-center">
-                        <button className='button-dev-center titulo'>Back-End</button>
-                        <button className='button-dev-center'>GitHub</button>
-                        <button className='button-dev-center'>GitHub</button>
-                        <button className='button-dev-center'>GitHub</button>
-                        <button className='button-dev-center'>GitHub</button>
-                        <button className='button-dev-center'>GitHub</button>
+                        <div className="button-wrapper">
+                            <button className='button-dev-center titulo'>Back-End</button>
+                            <div className="popup">Tecnologias e ferramentas usadas para gerenciar o servidor, banco de dados e lógica do aplicativo.</div>
+                        </div>
+                        <div className="button-wrapper">
+                            <button className='button-dev-center'>Node.js</button>
+                            <div className="popup">Ambiente de execução JavaScript do lado do servidor.</div>
+                        </div>
+                        <div className="button-wrapper">
+                            <button className='button-dev-center'>PHP</button>
+                            <div className="popup">Uma linguagem de programação popular para desenvolvimento web, usada com frameworks como Laravel e Symfony.</div>
+                        </div>
+                        <div className="button-wrapper">
+                            <button className='button-dev-center'>Python</button>
+                            <div className="popup">Linguagem de programação usada para desenvolvimento web, automação e análise de dados.</div>
+                        </div>
+                        <div className="button-wrapper">
+                            <button className='button-dev-center'>SQL</button>
+                            <div className="popup">Linguagem de programação padrão para gerenciamento e manipulação de bancos de dados relacionais.</div>
+                        </div>
+                        <div className="button-wrapper">
+                            <button className='button-dev-center'>C#</button>
+                            <div className="popup">Linguagem de programação moderna e orientada a objetos, desenvolvida pela Microsoft como parte da plataforma.</div>
+                        </div>
                     </div>
                     <div className="card-rigth">
-                        <button className='button-dev-rigth-titulo'>Framework</button>
-                        <button className='button-dev-rigth'>GitHub</button>
-                        <button className='button-dev-rigth'>GitHub</button>
-                        <button className='button-dev-rigth'>GitHub</button>
-                        <button className='button-dev-rigth'>GitHub</button>
-                        <button className='button-dev-rigth-button'>GitHub</button>
-
+                        <div className="button-wrapper">
+                            <button className='button-dev-rigth-titulo'>Frameworks</button>
+                            <div className="popup">Conjunto de ferramentas e bibliotecas para ajudar no desenvolvimento de aplicações web.</div>
+                        </div>
+                        <div className="button-wrapper">
+                            <button className='button-dev-rigth'>React.js</button>
+                            <div className="popup">Framework JavaScript para construção de interfaces de usuário, desenvolvida pelo Facebook. <br /> (OBS: Este portifólio é feito em React.js)</div>
+                        </div>
+                        <div className="button-wrapper">
+                            <button className='button-dev-rigth'>Bootstrap</button>
+                            <div className="popup">Framework CSS para desenvolvimento responsivo e móvel-first.</div>
+                        </div>
+                        <div className="button-wrapper">
+                            <button className='button-dev-rigth'>Django</button>
+                            <div className="popup">Framework web de alto nível para a linguagem de programação Python.</div>
+                        </div>
+                        <div className="button-wrapper">
+                            <button className='button-dev-rigth'>Laravel</button>
+                            <div className="popup">Framework web PHP conhecido por sua sintaxe expressiva e elegante.</div>
+                        </div>
+                        <div className="button-wrapper">
+                            <button className='button-dev-rigth-button'>Angular</button>
+                            <div className="popup">Plataforma de desenvolvimento para construir aplicações web com TypeScript.</div>
+                        </div>
                     </div>
                 </div>
             </Container>
+            <Container className="projects-section">
+                <div className="projects-buttons">
+                    <button
+                        className={`project-button b-p-direita ${selectedButton === 1 ? "active" : ""}`}
+                        onClick={() => handleButtonClick(1)}
+                    >
+                        1
+                    </button>
+                    <button
+                        className={`project-button ${selectedButton === 2 ? "active" : ""}`}
+                        onClick={() => handleButtonClick(2)}
+                    >
+                        2
+                    </button>
+                    <button
+                        className={`project-button b-p-esquerda ${selectedButton === 3 ? "active" : ""}`}
+                        onClick={() => handleButtonClick(3)}
+                    >
+                        3
+                    </button>
+                </div>
 
+                {/* Lista de projetos */}
+                <div className="projects-list">
+                    {projectsData[selectedButton].map((project) => (
+                        <Col key={project.id} md={6} className="project">
+                            <img src={project.image} alt={`Projeto ${project.id}`} className="project-image" />
+                            <div className="project-text">
+                                <p>{project.text}</p>
+                                <button className="saiba-mais">Saiba Mais</button>
+                            </div>
+                        </Col>
+                    ))}
+                </div>
+            </Container>
         </>
     );
 };
